512704297d5b44569213aa2151f8ae17e92a218f
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2024-09-29 18:06:48 +0000

    Add mathsrobotics folder

---
+++ mathsrobotics/unit1_intro/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+A = 36
+B = np.pi
+print('If A equals {}, its square root is {}'.format(A,np.sqrt(A)))
+print('If B equals the number PI ({}), the cosine of B is {}'.format(B,np.cos(B)))
